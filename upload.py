9e1c75ed7d5ba5406aea0770c1a4c3fcb2651c55
Shubham-Gaud <68564999+Shubham-Gaud@users.noreply.github.com>
2021-06-01 01:52:05 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -17,7 +17,7 @@ def contact():
 def about():
     return render_template("a.html")
 
-@app.route('/op', methods=['POST'])
+@app.route('/op', methods=['GET','POST'])
 def op():
     if request.method == 'POST':
         email = request.form.get("fname")
